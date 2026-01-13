123c18b8c4032ae1ce6dc62fbe744458e2d7d7a8
Dragoon Dorise <rodrigosedano@me.com>
2026-01-13 08:51:45 +0100

    no user config

--- livecd/airootfs/etc/archinstall/post_install.sh
+++ livecd/airootfs/etc/archinstall/post_install.sh
@@ -24,27 +24,9 @@ mkdir -p "$HOME/.config"
 echo "[KDE]" >> "$HOME/.config/kdeglobals"
 echo "LookAndFeelPackage=org.kde.breezedark.desktop" >> "$HOME/.config/kdeglobals"
 
-echo "$USER ALL=(ALL) NOPASSWD: /usr/bin/pacman" >> /etc/sudoers.d/99-pacman-nopasswd
-
-# yay (requiere tener go, git y base-devel instalados)
-su - "$USER" -c '
-  cd /tmp
-  git clone https://aur.archlinux.org/yay.git
-  cd yay
-  makepkg -si --noconfirm || true
-  cd /
-  rm -rf /tmp/yay
-  yay -S --noconfirm plymouth-theme-sweet-arch-git || true
-'
-
-plymouth-set-default-theme -R sweet-arch || true
-
 # Desktop shortcuts
 mkdir -p "$HOME/Desktop"
 cp /etc/archinstall/*.desktop "$HOME/Desktop/" || true
 chmod +x "$HOME/Desktop/"*.desktop || true
-
-# Permisos finales
-chown -R "$USER:$USER" "$HOME"
 
 exit 0
